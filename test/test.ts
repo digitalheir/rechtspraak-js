3535499fedeb5957f60a0c2a9ff451444408ae45
Maarten <maartentrompper@gmail.com>
2017-04-08 22:42:08 +0200

    make test automated

---
+++ test/test.ts
@@ -0,0 +1,28 @@
+import 'mocha';
+import RS from '../src/index';
+import * as fs from 'fs';
+import * as assert from "assert";
+
+describe('XML should convert to JSON-LD', function () {
+    it('should render', function () {
+        const xmlString = fs.readFileSync(
+            './test/test_docs/ECLI_NL_RBZWB_2016_1440.xml',
+            {encoding: 'utf8'}
+        );
+
+        const expectedJson = JSON.parse(fs.readFileSync(
+            './test/test_docs/ECLI_NL_RBZWB_2016_1440.jsonld',
+            {encoding: 'utf8'}
+        ));
+
+        // serialize & deserialize to get rid of fields set to undefined
+        let generatedJson = JSON.parse(JSON.stringify(RS.toJsonLdFromXmlString(xmlString)));
+        generatedJson.couchDbUpdated = expectedJson.couchDbUpdated;
+
+        assert.deepEqual(
+            generatedJson,
+            expectedJson
+        );
+    });
+
+});
