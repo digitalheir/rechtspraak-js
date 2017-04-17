568789afc66cd6204949f176e7187dfc6e3b9b4d
Maarten <maartentrompper@gmail.com>
2017-04-17 16:32:59 +0200

    v0.2.0

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
-export { toJsonLd, toJsonLdFromXmlString } from './lib/model/json-ld/to-json-ld';
-export * from './lib/model/rechtspraak_metadata';
-export { documentSchema } from './lib/model/json-ld/schema';
+export {toJsonLd, toJsonLdFromXmlString} from "./lib/model/json-ld/to-json-ld";
+export * from "./lib/model/rechtspraak_metadata";
+export {documentSchema} from "./lib/model/json-ld/schema";
+export {HTTPS_RECHTSPRAAK_LAWREADER, HTTPS_RECHTSPRAAK_LAWREADER_VOCAB} from "./lib/util/validations";
--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 var to_json_ld_1 = require("./lib/model/json-ld/to-json-ld");
 exports.toJsonLd = to_json_ld_1.toJsonLd;
 exports.toJsonLdFromXmlString = to_json_ld_1.toJsonLdFromXmlString;
 __export(require("./lib/model/rechtspraak_metadata"));
 var schema_1 = require("./lib/model/json-ld/schema");
 exports.documentSchema = schema_1.documentSchema;
+var validations_1 = require("./lib/util/validations");
+exports.HTTPS_RECHTSPRAAK_LAWREADER = validations_1.HTTPS_RECHTSPRAAK_LAWREADER;
+exports.HTTPS_RECHTSPRAAK_LAWREADER_VOCAB = validations_1.HTTPS_RECHTSPRAAK_LAWREADER_VOCAB;
 //# sourceMappingURL=index.js.map
